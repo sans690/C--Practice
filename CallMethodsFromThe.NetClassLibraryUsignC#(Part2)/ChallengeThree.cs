396e34246a1e3b591b52e92032d7d5dd9360430a
sans690 <scsmith7255@gmail.com>
2026-02-02 15:27:12 -0500

    Upload

---
+++ CallMethodsFromThe.NetClassLibraryUsignC#(Part2)/ChallengeThree.cs
@@ -0,0 +1,17 @@
+using System;
+
+class ChallengeThree
+{
+    static void Main(string[] args)
+    {
+        string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+        foreach (string orderID in orderIDs)
+        {
+            if (orderID.StartsWith("B"))
+            {
+                Console.WriteLine($"The order id starts with B: {orderID}");
+            }
+        }
+    }
+}
