7c9dc0d9fc14061cf35450b33ad8dce49440704f
sans690 <scsmith7255@gmail.com>
2026-02-05 13:33:10 -0500

    Upload (Final hit will happen)

--- AddLogicToC#ConsoleApplications(Part 3)/ChallengeSix.cs
+++ AddLogicToC#ConsoleApplications(Part 3)/ChallengeSix.cs
@@ -10,21 +10,18 @@ class ChallengeSix
         int monsterAttackValue = Random.Shared.Next(1, 11);
         do
         {
-            monsterHealth = monsterHealth - heroAttackValue;
-            Console.WriteLine($"Monster was damaged and lost {heroAttackValue} health and now has {monsterHealth} health.");
+            if (monsterHealth > 0)
+            {
+                monsterHealth = monsterHealth - heroAttackValue;
+                Console.WriteLine($"Monster was damaged and lost {heroAttackValue} health and now has {monsterHealth} health.");
+            }
 
-            heroHealth = heroHealth - monsterAttackValue;
-            Console.WriteLine($"Hero was damaged and lost {monsterAttackValue} health and now has {heroHealth} health.");
+            if (heroHealth > 0)
+            {
+                heroHealth = heroHealth - monsterAttackValue;
+                Console.WriteLine($"Hero was damaged and lost {monsterAttackValue} health and now has {heroHealth} health.");
+            }
         } while (monsterHealth > 0 && heroHealth > 0);
-
-        if (monsterHealth > 0)
-        {
-            Console.WriteLine("Monster wins!");
-        }
-
-        else if (heroHealth > 0)
-        {
-            Console.WriteLine("Hero. wins!");
-        }
+        Console.WriteLine(heroHealth > monsterHealth ? "Hero wins!" : "Monster wins!");
     }
 }
