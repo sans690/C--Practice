22484287939bbfbfd72313700b94c85697dfed96
sans690 <scsmith7255@gmail.com>
2026-03-06 20:41:10 -0500

    Upload

--- Program.cs
+++ Program.cs
@@ -5,6 +5,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        DebugApplicationPart6ChallengeFour.Run(args);
+        DebugApplicationPart6ChallengeFive.Run(args);
     }
 }
--- obj/Debug/net10.0/C# Practice.AssemblyInfo.cs
+++ obj/Debug/net10.0/C# Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C# Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+436cb0d429bdcbff25b5a45b8b01de6c317a9d55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7905111598dfc54103b95920b70c5d1bb967334f")]
 [assembly: System.Reflection.AssemblyProductAttribute("C# Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C# Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
